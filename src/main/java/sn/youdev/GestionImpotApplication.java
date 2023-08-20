1656af8b2aa6bcfdd9482cc2ee196d5830a93373
fayeyoussou <52853193+fayeyoussou@users.noreply.github.com>
2023-08-20 12:32:37 +0000

    try2

--- src/main/java/sn/youdev/GestionImpotApplication.java
+++ src/main/java/sn/youdev/GestionImpotApplication.java
@@ -2,10 +2,15 @@ package sn.youdev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GestionImpotApplication {
-
+public class GestionImpotApplication  extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(GestionImpotApplication.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(GestionImpotApplication.class, args);
     }
